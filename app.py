f7f351834a895052100f38578818e0c9621ac65b
keerthana-hacksmith <keerthana.hackathon@gmail.com>
2026-09-22 12:19:31 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+a=st.number_input("enter your number")
+b=st.number_input("enter another number")
+if st.button("add"):
+	st.success(a+b)
+elif st.button("subtract"):	
+	st.success(a-b)
+elif st.button("multipy"):
+	st.success(a*b)
